1bd1c52d6c567fb3616b2c5fccba1ca2eb72a707
jayanth1310 <90104768+jayanth1310@users.noreply.github.com>
2024-12-05 13:12:08 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -25,13 +25,6 @@ terraform {
       version = "~> 2.23.0"
     }
   }
-
-  backend "s3" {
-    bucket = "elasticbeanstalk-us-east-1-329599636889"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-
   required_version = "~> 1.10.1"
 }
 ##
