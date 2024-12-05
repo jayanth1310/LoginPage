13d504e4d0da8f8781d9e2da18e310f5b3510eeb
jayanth1310 <90104768+jayanth1310@users.noreply.github.com>
2024-12-05 11:39:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,17 +4,9 @@ terraform {
      source = "hashicorp/aws"
    }
  }
- 
-(*  backend "s3" {
-   region = "us-east-1"
-   key    = "terraform.tfstate"
- }
-} *)
- 
 provider "aws" {
  region = "us-east-1"
 }
- 
 resource "aws_instance" "test_instance" {
  ami           = "ami-005fc0f236362e99f"
  instance_type = "t2.nano"
