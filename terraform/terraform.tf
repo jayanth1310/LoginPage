29048788398c021039b8fcb3e4a448a659d9850f
jayanth1310 <90104768+jayanth1310@users.noreply.github.com>
2024-12-05 12:55:00 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsbucketjay21"
+    bucket = "elasticbeanstalk-us-east-1-329599636889"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
