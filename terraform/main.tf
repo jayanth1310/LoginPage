f8e3b50267dc8d35696fb8a0efd423f1c852a96c
jayanth1310 <90104768+jayanth1310@users.noreply.github.com>
2024-12-05 11:41:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,22 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Specify the provider version if needed
+    }
+  }
+}
+
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
+
 resource "aws_instance" "test_instance" {
- ami           = "ami-005fc0f236362e99f"
- instance_type = "t2.nano"
- tags = {
-   Name = "test_instance"
- }
+  ami           = "ami-005fc0f236362e99f"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "test_instance"
+  }
 }
+
